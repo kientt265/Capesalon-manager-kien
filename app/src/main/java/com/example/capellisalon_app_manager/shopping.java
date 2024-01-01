77f2bc594c8dae51d1e77ec744e9f076577ae9c3
kientt265 <keoxinhzai@gmail.com>
2024-01-02 00:31:55 +0700

    UI-intent

---
+++ app/src/main/java/com/example/capellisalon_app_manager/shopping.java
@@ -0,0 +1,13 @@
+package com.example.capellisalon_app_manager;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class shopping extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shopping);
+    }
+}
